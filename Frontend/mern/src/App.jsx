b2586ec4f92689f994ed1f010d596bcdad0cbdcf
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-04-19 09:59:48 +0530

    fix: fix App.jsx

--- Frontend/mern/src/App.jsx
+++ Frontend/mern/src/App.jsx
@@ -4,7 +4,7 @@ import Login from './Auth/pages/Login'
 import SignUp from './Auth/pages/SignUp'
 import Protected from './Auth/components/Protected'
 import Home from './interview/pages/Home'
-import Interview from './interview/pages/interview'
+import Interview from './interview/pages/Interview'
 import AllReports from './interview/pages/AllReports'
 
 const App = () => {
