2a00752a7aa815724a9d38c97eb9e96d4d78be4f
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-04-19 09:53:21 +0530

    build: integrate frontend dist with backend for production deployment

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser"
 import cors from 'cors'
 import authRouter from './routes/authRoutes.js'
 import interviewRouter from './routes/interview.routes.js'
+import path from 'path'
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
@@ -10,6 +11,12 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
+const __dirname = path.resolve()
 app.use('/api/auth',authRouter)
 app.use('/api/interview/',interviewRouter)
+app.use(express.static(path.join(__dirname,"Frontend/mern/dist")))
+app.get("/{*splat}",(req,resp)=>{
+    resp.sendFile(path.resolve(__dirname,"Frontend","mern","dist","index.html"))
+})
+
 export default app
--- Backend/server.js
+++ Backend/server.js
@@ -14,8 +14,10 @@
 
 import 'dotenv/config'
 import app from "./app.js";
+
 import { connectDB } from './config/db.js';
 connectDB()
+
 app.listen(process.env.PORT,()=>{
     console.log("Server is running on port 3000")
 })
